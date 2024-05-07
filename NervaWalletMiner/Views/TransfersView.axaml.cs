37924d2a148fa8cd71b072c5d00af71836a606a4
R0BC0D3R <R0BC0D3R@gmail.com>
2024-05-07 09:21:32 -0400

    Remove Open Wallet button from Transfers screen

--- NervaWalletMiner/Views/TransfersView.axaml.cs
+++ NervaWalletMiner/Views/TransfersView.axaml.cs
@@ -1,7 +1,4 @@
 using Avalonia.Controls;
-using Avalonia.Interactivity;
-using NervaWalletMiner.Helpers;
-using System;
 
 namespace NervaWalletMiner.Views
 {
@@ -10,18 +7,6 @@ namespace NervaWalletMiner.Views
         public TransfersView()
         {
             InitializeComponent();
-        }
-
-        public void OpenCloseWalletClicked(object sender, RoutedEventArgs args)
-        {
-            try
-            {
-                // TODO: Implement this in a different way so it works for both Wallet and Transfers screen
-            }
-            catch (Exception ex)
-            {
-                Logger.LogException("Trans.SSMC", ex);
-            }
         }
     }
 }
