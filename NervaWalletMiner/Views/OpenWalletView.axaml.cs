2bf8a8826cbc0f17d6bb1e85ab78bcd381e18616
R0BC0D3R <R0BC0D3R@gmail.com>
2024-05-14 09:37:18 -0400

    Finish Open Wallet screen so it can be used as template for other pop-ups

--- NervaWalletMiner/Views/OpenWalletView.axaml.cs
+++ NervaWalletMiner/Views/OpenWalletView.axaml.cs
@@ -54,28 +54,67 @@ namespace NervaWalletMiner.Views
 
         public void OkButtonClicked(object sender, RoutedEventArgs args)
         {
-            var cbxWallet = this.Get<ComboBox>("cbxWalletName");
-            var tbxPassword = this.Get<TextBox>("tbxPassword");
-
-            DialogResult result = new()
+            try
             {
-                IsOk = true,
-                WalletName = cbxWallet.SelectedValue?.ToString(),
-                WalletPassword = tbxPassword.Text
-            };
+                var cbxWallet = this.Get<ComboBox>("cbxWalletName");
+                var tbxPassword = this.Get<TextBox>("tbxPassword");
 
-            Close(result);
+                DialogResult result = new()
+                {
+                    IsOk = true,
+                    WalletName = cbxWallet.SelectedValue?.ToString(),
+                    WalletPassword = tbxPassword.Text
+                };
+
+                Close(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException("OWal.OBC", ex);
+            }
         }
 
         public void CancelButtonClicked(object sender, RoutedEventArgs args)
         {
-            DialogResult result = new()
+            try
             {
-                IsCancel = true
-            };
+                DialogResult result = new()
+                {
+                    IsCancel = true
+                };
 
-            Close(result);
+                Close(result);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException("OWal.CBC", ex);
+            }
+        }
 
+        public void ShowHidePasswordButtonClicked(object sender, RoutedEventArgs args)
+        {
+            try
+            {
+                var tbxPassword = this.Get<TextBox>("tbxPassword");
+                var btnShowHidePassword = this.Get<Button>("btnShowHidePassword");
+
+                if (tbxPassword.RevealPassword)
+                {
+                    // Reveal was true, so hide
+                    tbxPassword.RevealPassword = false;
+                    btnShowHidePassword.Content = "Show";
+                }
+                else
+                {
+                    // Reveal was false, so show
+                    tbxPassword.RevealPassword = true;
+                    btnShowHidePassword.Content = "Hide";
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogException("OWal.SHPBC", ex);
+            }
         }
     }
 }
