cd3080290871f7ae257a523436d8830395d38a3e
R0BC0D3R <R0BC0D3R@gmail.com>
2026-04-28 12:40:59 -0400

    Disable Android debugging and set release version 1.0.0

--- NervaOneWalletMiner/Helpers/GlobalData.cs
+++ NervaOneWalletMiner/Helpers/GlobalData.cs
@@ -16,7 +16,7 @@ namespace NervaOneWalletMiner.Helpers
     {
         public const string AppNameMain = "NervaOne";
         public const string AppAssemblyName = "NervaOneWalletMiner";
-        public const string Version = "0.8.5.15";
+        public const string Version = "1.0.0";
 
         public const string CliToolsDirName = "cli";
         public const string WalletDirName = "wallets";
